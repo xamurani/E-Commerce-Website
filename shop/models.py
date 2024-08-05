d213bbb14da7596e0e271080a76aaceb53343bba
Muhammad <122938837+xamurani@users.noreply.github.com>
2024-08-05 17:59:04 +0500

    Update models.py

--- shop/models.py
+++ shop/models.py
@@ -9,7 +9,7 @@ class Product(models.Model):
     sub_category = models.CharField(max_length=50, default="")
     price = models.IntegerField(default=0)
     desc = models.CharField(max_length=300)
-    pub_date = models.DataField()
+    pub_date = models.DateField()
     image = models.ImageField(upload_to="shop/images", default="")
 
     def __str__(self):
