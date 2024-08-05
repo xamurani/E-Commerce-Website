9a92e9a9fb698471796c6a3b03e4ab66f62ffe29
Muhammad <122938837+xamurani@users.noreply.github.com>
2024-08-05 16:44:36 +0500

    Update settings.py

--- myawesomestore/settings.py
+++ myawesomestore/settings.py
@@ -128,5 +128,4 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
