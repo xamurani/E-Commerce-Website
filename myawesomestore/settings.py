1e74beebb26633819adbaeb45a5856af53500910
Muhammad <122938837+xamurani@users.noreply.github.com>
2024-08-05 16:20:15 +0500

    Update settings.py

--- myawesomestore/settings.py
+++ myawesomestore/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6h!5p!_tr6o6sotj^es5$5yyxa+7us1&d2-*xkh$k-q-o@496!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
