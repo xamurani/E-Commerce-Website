52a7e06d3fd4d47107525b8d2853a1f1773a4e65
Muhammad <122938837+xamurani@users.noreply.github.com>
2024-08-07 15:38:21 +0500

    Miragted the database to PostgresSQL

--- myawesomestore/settings.py
+++ myawesomestore/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6h!5p!_tr6o6sotj^es5$5yyxa+7us1&d2-*xkh$k-q-o@496!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['xamurani.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -75,12 +75,26 @@ WSGI_APPLICATION = 'myawesomestore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',  # Replace with your database name
+        'USER': 'postgres',  # Replace with your database user
+        'PASSWORD': '1234',  # Replace with your database password
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
@@ -128,4 +142,5 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
